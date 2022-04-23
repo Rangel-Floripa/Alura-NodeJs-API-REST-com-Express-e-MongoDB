d7b887cbbc5b1550c5456f93cf0c198cfdcf0582
Rangel-Floripa <rangelflorianopolis@gmail.com>
2022-04-23 06:37:35 -0300

    nodemon, rotas, gitignore

--- alura-node/server.js
+++ alura-node/server.js
@@ -1,9 +1,18 @@
 const http = require("http")
 const port = 3000;
 
+const rotas = {
+    '/': 'Curso de Node',
+    '/livros': 'Entrei na pag de livros',
+    '/autores': 'Listagem de autotes',
+    '/editora': 'Pag de editora',
+    '/sobre': 'Inf sobre o projeto'
+
+}
+
 const server = http.createServer((req, res) => {
     res.writeHead(200, {'Content-type':'text/plain'});
-    res.end('Curso de Node');
+    res.end(rotas[req.url]);
 })
 
 server.listen(port, () => {
