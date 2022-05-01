3a1aac0944d6a937eb46e1314dd9a7bbb2e4e9de
Rangel-Floripa <rangelflorianopolis@gmail.com>
2022-05-01 09:50:27 -0300

    criar servidor local com express

--- alura-node/server.js
+++ alura-node/server.js
@@ -1,21 +1,7 @@
-const http = require("http")
-const port = 3000;
+import app from './src/app.js'
 
-const rotas = {
-    '/': 'Curso de Node',
-    '/livros': 'Entrei na pag de livros',
-    '/autores': 'Listagem de autotes',
-    '/editora': 'Pag de editora',
-    '/sobre': 'Inf sobre o projeto'
-
-}
-
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-type':'text/plain'});
-    res.end(rotas[req.url]);
-})
-
-server.listen(port, () => {
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
     console.log(`Servidor escutando em http://localhost:${port}`)
     
 })
